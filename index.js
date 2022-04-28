4de68184a0ddd62ec5284ad7ac83d96639ef24bd
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-04-29 00:57:02 +0600

    add .evn file

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port =process.env.PORT || 5000
+const express = require('express');
+var cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u8fjq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+
+    client.close();
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
