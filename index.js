0258c7109f1dd7adb21211df2785bf939e67e081
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-04-30 23:08:08 +0600

    add delete API

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ async function run() {
             res.send(result);
 
         });
+
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        });
+
         app.get('/user/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
