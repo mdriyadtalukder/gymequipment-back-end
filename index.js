0215dc4e5847698c3fce94cb24361f4306b2cb5c
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-05-01 23:51:23 +0600

    edit code

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ function verifyJWT(req, res, next) {
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const { add } = require('nodemon/lib/rules');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u8fjq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
